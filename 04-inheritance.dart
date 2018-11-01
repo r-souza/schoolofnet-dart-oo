a57618ff84d2affd0377e76135d912581ed885df
Rodrigo de Souza <rodrigo.evildead@gmail.com>
2018-10-31 23:09:08 -0300

    Inheritance

---
+++ 04-inheritance.dart
@@ -0,0 +1,34 @@
+class Person {
+  String firstName, lastName;
+
+  Person(this.firstName, this.lastName);
+
+  String getFullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+}
+
+class Employee extends Person{
+  double salary;
+
+  Employee(String firstName, String lastName, this.salary)
+    : super(firstName, lastName);
+  
+  void increateSalary(double amount) {
+    this.salary += amount;
+  }
+  
+}
+
+void main() {
+  Person person = new Person('Rodrigo', 'de Souza');
+  print(person.getFullName());
+
+  Employee employee = new Employee('Samira', 'de Lima', 2000.0);
+  print(employee.getFullName());
+  print('Salary = ' + employee.salary.toString());
+
+  employee.increateSalary(1250.00);
+
+  print('Salary = ' + employee.salary.toString());
+}
